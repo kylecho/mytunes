4e9cb2531900ceb786cb4a8ed7c8ee35b7294030
Fred Zirdung <fred@hackreactor.com>
2015-09-01 09:15:17 -0700

    (init)

---
+++ client/views/SongQueueView.js
@@ -0,0 +1,12 @@
+// SongQueueView.js - Defines a backbone view class for the song queue.
+var SongQueueView = Backbone.View.extend({
+
+  initialize: function() {
+  },
+
+  render: function() {
+  initialize: function() {
+  },
+  }
+
+});
